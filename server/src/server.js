5987026faa2fb95a698d552906d4e003f4bf9d0b
imTratax <justin.ledgard@gmail.com>
2022-01-14 15:46:43 +1100

    Added express

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,22 @@
 const WebSocket = require("ws");
+const express = require("express");
+const app = express();
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 5000 });
 
-console.log(`Server started.`);
+console.log(`Express Started`);
+app.get('/', function (req, res) {
+    res.send('Hello World');
+})
 
+var server = app.listen(8080, function () {
+    var host = "127.0.0.1"
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+})
+
+
+console.log("WebSocket Server started");
 try {
     wss.on('connection', function connection(ws) {
         console.log(`A new websocket session has established`);
